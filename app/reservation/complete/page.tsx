f3168f1a194fc58cff707e1b9a5ea9de030921f5
Hyunjun Choi <chj7239@gmail.com>
2024-03-20 14:33:02 +0900

    ui: 완료 페이지 문구 변경

--- app/reservation/complete/page.tsx
+++ app/reservation/complete/page.tsx
@@ -4,17 +4,15 @@ export default function Page() {
     return (
         <div>
           <div className="w-full h-1 bg-[#D9D9D9]">
-            <div className="w-[93vw] h-1 bg-highlight" />
+            <div className="w-[100vw] h-1 bg-highlight" />
           </div>
     
           <div className="ml-6 mt-6 mb-7">
             <Headline
               title="기사님께 의뢰서를 보냈어요 🎉"
-              description="기사님에게 응답이 올 때까지 천천히 기다려주세요"
+              description="기사님에게 응답이 오면 문자메시지로 알려드릴게요"
             />
           </div>
-    
-          
         </div>
       );
     }
