0f693f17d3fae667ecc35a512f630f87c839f0fd
Hyunjun Choi <chj7239@gmail.com>
2024-03-20 23:41:10 +0900

    chore: 불필요한 import 제거

--- app/user/bill/[token]/page.tsx
+++ app/user/bill/[token]/page.tsx
@@ -1,6 +1,6 @@
-import { generate, verifyOrNull } from '@/app/lib/auth/jwt';
-import { RepairRepository } from '@/app/lib/repository/repair-repository';
-import RepairRepositoryImpl from '@/app/lib/repository/repair-repository-impl';
+import { verifyOrNull } from "@/app/lib/auth/jwt";
+import { RepairRepository } from "@/app/lib/repository/repair-repository";
+import RepairRepositoryImpl from "@/app/lib/repository/repair-repository-impl";
 
 export default async function Page({ params }: { params: { token: string } }) {
   const repairRepository: RepairRepository = new RepairRepositoryImpl();
@@ -8,14 +8,17 @@ export default async function Page({ params }: { params: { token: string } }) {
   const payload: any = verifyOrNull(params.token);
   let repairRequest = null;
 
-  if (payload !== null && payload.hasOwnProperty('requestId')) {
-    repairRequest = await repairRepository.getRepairRequestOrNull(payload.requestId);
+  if (payload !== null && payload.hasOwnProperty("requestId")) {
+    repairRequest = await repairRepository.getRepairRequestOrNull(
+      payload.requestId
+    );
   }
 
-  return (<div>
-    <div>My Token: {params.token}</div>
-    <div>Payload: {JSON.stringify(payload)} </div>
-    <div>Request: {JSON.stringify(repairRequest)} </div>
+  return (
+    <div>
+      <div>My Token: {params.token}</div>
+      <div>Payload: {JSON.stringify(payload)} </div>
+      <div>Request: {JSON.stringify(repairRequest)} </div>
     </div>
   );
 }
